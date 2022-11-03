20dca912f7ab426ec2d819b7f41cf7ed9bdf1526
sarahffm <sarahboeckel999@gmail.com>
2022-11-03 18:32:42 +0100

    changed status code

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Controllers/SessionController.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Controllers/SessionController.cs
@@ -23,7 +23,7 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
 
         [HttpPut]
         [AllowAnonymous]
-        [ProducesResponseType(typeof(bool), StatusCodes.Status202Accepted)]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -34,7 +34,7 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
 
             try
             {
-                return Accepted(await _sessionService.InvalidateSession(id));
+                return Ok(await _sessionService.InvalidateSession(id));
             }
             catch (Exception exception)
             {
