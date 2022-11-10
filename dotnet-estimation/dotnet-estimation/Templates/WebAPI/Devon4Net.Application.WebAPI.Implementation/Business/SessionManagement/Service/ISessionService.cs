c2babdc78963a867a7828a4df079eb3ae4eeafd6
Zina Christensen <103071518+zichriste@users.noreply.github.com>
2022-11-10 08:59:50 +0100

    Added Leave-Session-Service (#12)
    * added service which enables user to leave a session
    
    * added nullchecks
    
    * Removed unnecessary exception

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/ISessionService.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/ISessionService.cs
@@ -13,5 +13,7 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
         public Task<(bool, Devon4Net.Application.WebAPI.Implementation.Domain.Entities.Task?)> GetStatus(long sessionId);
 
         public Task<Estimation> AddNewEstimation(long sessionId , string VoteBy, int Conplexity);
+    
+        public Task<bool> RemoveUserFromSession(long id, String userId);
     }
 }
