36dca534d32b8e4b274b479bd54272399e8a80c9
Othman Moukhli <othman.moukhli@capgemini.com>
2022-11-16 12:57:19 +0100

    SessionDto small correction

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Dtos/SessionDto.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Dtos/SessionDto.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Devon4Net.Application.WebAPI.Implementation.Domain.Entities;
 using Devon4Net.Application.WebAPI.Implementation;
-namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Dto
+namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Dtos
 {
     /// <summary>
     /// Session definition
