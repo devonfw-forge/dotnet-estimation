8d7c558473fcca723dbd8f933a5fef629a4d2305
Othman Moukhli <othman.moukhli@capgemini.com>
2022-11-16 10:40:17 +0100

    conflicts resolved for sessionservice

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/SessionService.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/SessionService.cs
@@ -51,17 +51,37 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
             // FIXME: LiteDb also returs null values, when a matching entity does not exist!
             return _sessionRepository.GetFirstOrDefault(expression);
         }
+        
+        public async Task<bool> InvalidateSession(long sessionId)
+        {
+            Session sessionResult = await GetSession(sessionId);
 
-        public async Task<(bool, Domain.Entities.Task?)> GetStatus(long sessionId) {
-            var sessionResult = await GetSession(sessionId);
-            
             if (sessionResult == null)
             {
                 throw new NotFoundException(sessionId);
             }
 
-            bool sessionIsValid = sessionResult.isValid();
-            
+            if (!sessionResult.IsValid())
+            {
+                throw new InvalidSessionException(sessionId);
+            }
+
+            sessionResult.ExpiresAt = DateTime.Now;
+
+            return _sessionRepository.Update(sessionResult);
+        }
+        
+        public async Task<(bool, Domain.Entities.Task?)> GetStatus(long sessionId)
+        {
+            var sessionResult = await GetSession(sessionId);
+
+            if (sessionResult == null)
+            {
+                throw new NotFoundException(sessionId);
+            }
+
+            bool sessionIsValid = sessionResult.IsValid();
+
             if (!sessionIsValid)
             {
                 return (false, null);
@@ -70,22 +90,23 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
             // since there can be only one task which is being evaluated,
             // we only query the first object
             var evaluatedTask = sessionResult.Tasks.ToList().Find(item => item.Status == Status.Evaluated);
-            
-            if (evaluatedTask is not null) {
+
+            if (evaluatedTask is not null)
+            {
                 return (sessionIsValid, evaluatedTask);
             }
 
             // else we try to find tasks which are open
             var openTasks = sessionResult.Tasks.Where(item => item.Status == Status.Open).ToList();
 
-            if (openTasks.Any()) 
+            if (openTasks.Any())
             {
                 openTasks.Sort((x, y) => DateTime.Compare(x.CreatedAt, y.CreatedAt));
 
                 var currentTask = openTasks.First();
 
                 return (sessionIsValid, currentTask);
-            } 
+            }
             else
             {
                 // if there are no open tasks left to be estimated we query for postponed tasks
@@ -102,6 +123,114 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
             }
 
             return (sessionIsValid, null);
+        }
+        public async Task<Estimation> AddNewEstimation(long sessionId, string VoteBy, int Complexity)
+        {
+            var (isvalid, currentTask) = await GetStatus(sessionId);
+            if (!isvalid)
+            {
+                throw new NoLongerValid(sessionId);
+            }
+            if (currentTask == null)
+            {
+                throw new NoOpenOrSuspendedTask();
+            }
+
+            var newEstimation = new Estimation { VoteBy = VoteBy, Complexity = Complexity };
+
+            var session = await GetSession(sessionId);
+
+            for (int i = 0; i < session.Tasks.Count; i++)
+            {
+                if (session.Tasks[i].Id == currentTask.Id)
+                {
+                    session.Tasks[i].Estimations.Add(newEstimation);
+                }
+            }
+
+            _sessionRepository.Update(session);
+
+            return newEstimation;
+        }
+        public async Task<bool> RemoveUserFromSession(long sessionId, string userId)
+        {
+            var expression = LiteDB.Query.EQ("_id", sessionId);
+            var session = _sessionRepository.GetFirstOrDefault(expression);
+
+            if(session != null) {
+                var user = session.Users.SingleOrDefault(i => i.Id == userId);
+
+                if (user != null) {
+                    session.Users.Remove(user);
+                    _sessionRepository.Update(session);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Add an user to a given session
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<bool> AddUserToSession(long sessionId, string userId, Role role)
+        {
+            var expression = LiteDB.Query.EQ("_id", sessionId);
+            var session = _sessionRepository.GetFirstOrDefault(expression);
+            var newUser = new User
+            {
+                Id = userId,
+                Role = role,
+            };
+
+            if (session != null)
+            {
+                if (!session.Users.Any(x => x.Equals(newUser)))
+                {
+                    session.Users.Add(newUser);
+                    return _sessionRepository.Update(session);
+                }
+            }
+            return false;
+        }
+        public async Task<bool> AddTaskToSession(long sessionId, TaskDto task)
+        {
+           /* var newSession = new Session
+            {
+                Id = 1,
+                InviteToken = "asdf",
+                ExpiresAt = DateTime.Now.AddMinutes(25),
+                Tasks = new List<Domain.Entities.Task>(),
+                Users = new List<Domain.Entities.User>(),
+            };
+            _sessionRepository.Create(newSession);
+           */
+            var expression = LiteDB.Query.EQ("_id", sessionId);
+            var session = _sessionRepository.GetFirstOrDefault(expression);
+            var (id, title, description, url, status) = task;
+
+            var newTask = new Domain.Entities.Task
+            {
+                Id = id,
+                Title = title,
+                Description = description,
+                Url = url,
+                Status = status
+            };
+
+            if (session != null)
+            {
+                if (!session.Tasks.Any(x => x.Equals(newTask)))
+                {
+                    session.Tasks.Add(newTask);
+
+                    return _sessionRepository.Update(session);
+                }
+            }
+            return false;
         }
 
         private string generateInviteToken()
