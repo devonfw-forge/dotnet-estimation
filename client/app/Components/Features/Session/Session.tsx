b3133f208ee9c905e4cfc1ee9ce576268c544473
BenjaminE <benjamin.ehnes@fh-erfurt.de>
2022-11-23 13:05:11 +0100

    trying to fix the bug, where we cant initially load tasks

--- client/app/Components/Features/Session/Session.tsx
+++ client/app/Components/Features/Session/Session.tsx
@@ -2,25 +2,21 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useWebSocket from "react-use-websocket";
-import { Estimation } from "../../app/Components/Features/Session/Estimation/Components/Estimation";
-import { TaskView } from "../../app/Components/Features/Session/Tasks/Components/TaskView";
-import { useTaskStore } from "../../app/Components/Features/Session/Tasks/Stores/TaskStore";
-import { UserView } from "../../app/Components/Features/Session/Users/Components/UserView";
-import { App } from "../../app/Components/Globals/App";
-import { Frame } from "../../app/Components/Globals/Frame";
-import { StickyHeader } from "../../app/Components/Globals/StickyHeader";
-import { baseUrl, serviceUrl } from "../../app/Constants/url";
-import { IMessage, ITypedMessage } from "../../app/Interfaces/IMessage";
-import { ITask, ITaskStatusChange } from "../../app/Interfaces/ITask";
-import { IWebSocketMessage } from "../../app/Interfaces/IWebSocketMessage";
-import { Type } from "../../app/Types/Type";
+import { baseUrl, serviceUrl } from "../../../Constants/url";
+import { ITypedMessage, IMessage } from "../../../Interfaces/IMessage";
+import { ITask, ITaskStatusChange } from "../../../Interfaces/ITask";
+import { IWebSocketMessage } from "../../../Interfaces/IWebSocketMessage";
+import { Type } from "../../../Types/Type";
+import { App } from "../../Globals/App";
+import { Frame } from "../../Globals/Frame";
+import { StickyHeader } from "../../Globals/StickyHeader";
+import { Estimation } from "./Estimation/Components/Estimation";
+import { TaskView } from "./Tasks/Components/TaskView";
+import { useTaskStore } from "./Tasks/Stores/TaskStore";
+import { UserView } from "./Users/Components/UserView";
 
-export default function Session({ id, data }: any) {
-  const { fetch, setCurrentTasks } = useTaskStore();
-
-  useEffect(() => {
-    setCurrentTasks(data.Tasks);
-  }, [data]);
+export default function Session({ id }: any) {
+  const store = useTaskStore();
 
   //  process onUserConnect, onAnotherUserConnect, markTaskAsActive,
   const processMessage = (message: IWebSocketMessage) => {
@@ -80,22 +76,4 @@ export default function Session({ id, data }: any) {
       </Frame>
     </>
   );
-}
-
-export async function getServerSideProps(context: any) {
-  const { params } = context;
-
-  const { id } = params;
-
-  const res = await fetch(
-    "http://127.0.0.1:8085/estimation/v1/session/" + id + "/status"
-  );
-
-  const data = await res.json();
-
-  console.log(data);
-
-  return {
-    props: { id, data }, // will be passed to the page component as props
-  };
 }
