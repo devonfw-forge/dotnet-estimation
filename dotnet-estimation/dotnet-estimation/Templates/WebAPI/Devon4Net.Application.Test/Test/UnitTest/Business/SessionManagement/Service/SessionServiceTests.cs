c3a19d106154f61574474cb8cc2b649e2545c91b
Vladislav Sehtman <vlad96@hotmail.de>
2022-11-25 08:58:26 +0100

    Deleted unnecessary Controller Testfile since it wasnt implemented yet. Added some testcases especially for the ChangeTaskStatus Method of ISessionService

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.Test/Test/UnitTest/Business/SessionManagement/Service/SessionServiceTests.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.Test/Test/UnitTest/Business/SessionManagement/Service/SessionServiceTests.cs
@@ -3,12 +3,10 @@ using Xunit;
 using Moq;
 using Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Service;
 using Devon4Net.Application.WebAPI.Implementation.Domain.Entities;
-using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using Task = Devon4Net.Application.WebAPI.Implementation.Domain.Entities.Task;
 using Devon4Net.Infrastructure.LiteDb.Repository;
-using Devon4Net.Infrastructure.Test;
 using FluentAssertions;
 using Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Dtos;
 using Xunit.Abstractions;
@@ -17,7 +15,6 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Management.Controllers
 {
     public class SessionServiceTests : UnitTest
     {
-        //private readonly Mock<ISessionRepository> repositoryStub = new();
         private readonly Mock<ILiteDbRepository<Session>> repositoryStub = new Mock<ILiteDbRepository<Session>>();
         private readonly Random rnd = new();
         private readonly ITestOutputHelper output;
@@ -26,192 +23,219 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Management.Controllers
         {
             this.output = output;
         }
-        /*
+        
         [Fact]
-        public async Task<Object> GetSession_WithPopulatedRepo_ReturnsAllSessions()
+        public async void GetSession_WithPopulatedRepo_ReturnsTheSearchedSession()
         {
             //Arrange
-            var ExpectedSessions = new[] { CreateRandomSession(1), CreateRandomSession(2), CreateRandomSession(3) };
-            repositoryStub.Setup(repo => repo.Get())
-                .Returns(ExpectedSessions);
+            var ExpectedSession = CreateRandomSession(2);
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
+                .Returns(ExpectedSession);
 
-            var controller = new SessionService(repositoryStub.Object);
+            var sessionService = new SessionService(repositoryStub.Object);
+
             //Act
-            var actualSessions = await controller.GetSession(1);
-            Console.WriteLine("Expected Sessions[0]:\n" + ExpectedSessions[0]);
-            Console.WriteLine("Expected Sessions:\n" + ExpectedSessions);
-            Console.WriteLine("Actual Session:\n" + actualSessions);
+            var actualSession2 = await sessionService.GetSession(2);
+
             //Assert
-            return actualSessions.Should().BeEquivalentTo(ExpectedSessions[0], options => options.ComparingByMembers<Session>());
+            actualSession2.Should().BeEquivalentTo(ExpectedSession, options => options.ComparingByMembers<Session>());
         }
-        */
+        
 
-        [Fact]
-        public async void ChangeTaskStatus_WithSameStatus_ReturnsNoStatusChanges()
+        [Theory]
+        [InlineData(1, "Task1", Status.Open)]
+        [InlineData(1, "Task1", Status.Evaluated)]
+        [InlineData(1, "Task1", Status.Suspended)]
+        [InlineData(1, "Task1", Status.Ended)]
+        public async void ChangeTaskStatus_WithSameStatus_ReturnsNoStatusChanges(long sessionId, string statusId, Status status)
         {
             //Arrange
-            var InitialSession = new[] { CreateRandomSession(1) };
-            repositoryStub.Setup(repo => repo.Get())
-                .Returns(InitialSession);
-
-            var Controller = new SessionService(repositoryStub.Object);
-            var firstTask = InitialSession[0].Tasks[0];
-
-            output.WriteLine("Hello" + firstTask);
-            //Act
-            var newStatus = new TaskStatusChangeDto()
+            var expectedTaskStatusChangeDtoResult = new List<TaskStatusChangeDto>() { };
+            var destinationStatus = new TaskStatusChangeDto()
             {
-                Id = firstTask.Id,
-                Status = firstTask.Status,
+                Id = statusId,
+                Status = status
             };
 
-            var (TaskChanged, modifiedTasks) = await Controller.ChangeTaskStatus(1, newStatus);
+            //Arrange the Mock Repository with a Session
+            var repoStub = new Mock<ILiteDbRepository<Session>>();
+            var InitialSession = CreateRandomSession(1);
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
+                .Returns(InitialSession);
+
+            var service = new SessionService(repositoryStub.Object);
+
+            //Act
+            var (modified, modifiedTasks) = await service.ChangeTaskStatus(sessionId, destinationStatus);
+
             //Assert
-            Assert.False(TaskChanged);
+            //Assert that the delivered indicator whether changes were made is false, since no changes are expected
+            Assert.False(modified);
+            //Assert that the list of modified TaskStatusChangeDtos is empty
+            modifiedTasks.Should().BeEquivalentTo(expectedTaskStatusChangeDtoResult, options => options.ComparingByMembers<TaskStatusChangeDto>());
         }
 
-        [Fact]
-        public async void ChangeTaskStatus_OpenToSuspended_ReturnsTheChangedTaskStatus()
+        
+        [Theory]
+        [InlineData(1, "Task1", Status.Suspended)]
+        public async void ChangeTaskStatus_OpenToSuspended_ReturnsTheChangedTaskStatus(long sessionId, string statusId, Status status)
         {
             //Arrange
-            var InitialSession = new[] { CreateRandomSession(1) };
-            var firstTask = InitialSession[0].Tasks[0];
+            //Arrange the expected Result
+            var expectedTaskStatusChangeDtoResult = new List<TaskStatusChangeDto>() { new TaskStatusChangeDto () { Id = statusId, Status = status } };
+            var destinationStatus = new TaskStatusChangeDto()
+            {
+                Id = statusId,
+                Status = status
+            };
 
-            //Set the first Task Status to Open
+            //Arrange the Mock Repository with a Session
+            var repoStub = new Mock<ILiteDbRepository<Session>>(MockBehavior.Strict);
+            var InitialSession = CreateRandomSession(1);
+            var firstTask = InitialSession.Tasks[0];
+            firstTask.Id = statusId;
             firstTask.Status = Status.Open;
 
-            repositoryStub.Setup(repo => repo.Get())
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
                 .Returns(InitialSession);
 
-            var Controller = new SessionService(repositoryStub.Object);
+            repositoryStub.Setup(repo => repo.Update(
+                It.IsAny<Session>()
+            ))
+                .Returns(true);
+
+            var service = new SessionService(repositoryStub.Object);
 
             //Act
-            var newStatusSuspended = new TaskStatusChangeDto()
-            {
-                Id = firstTask.Id,
-                Status = Status.Suspended,
-            };
+            var (modified, modifiedTasks) = await service.ChangeTaskStatus(sessionId, destinationStatus);
 
-            var (TaskChanged, modifiedTasks) = await Controller.ChangeTaskStatus(1, newStatusSuspended);
-            Assert.False(TaskChanged);
-
-            //Assert that the TaskStatusChangeDto delivers a suspended Status
-            foreach (var task in modifiedTasks)
-            {
-                if(task.Id == firstTask.Id)
-                {
-                    //resultTaskStatusChangeDto = task.Status.Should().Equals(Status.Suspended);
-                    //resultTaskStatusChangeDto =
-                    Assert.True(task.Status == Status.Suspended);
-                }
-            }
-            output.WriteLine("Hello" + modifiedTasks.Count);
-            //TODO : Check whether the changes took also place in the database.
-            //Assert that the Task's status inside the Database was changed to suspended
-            //Assert.True(firstTask.Status != Status.Open);
-            //var updatedSessionRepo = await Controller.GetSession(1);
-            //Assert.NotNull(updatedSessionRepo);
-            //Assert.True(updatedSessionRepo.Tasks.Where(task => task.Id == firstTask.Id && task.Status == Status.Suspended).Any());
+            //Assert 
+            //Assert that the delivered indicator whether changes were made is false, since no changes are expected
+            Assert.True(modified);
+            //Assert that the list of modified TaskStatusChangeDtos delivers a suspended Status
+            modifiedTasks.Should().BeEquivalentTo(expectedTaskStatusChangeDtoResult, options => options.ComparingByMembers<TaskStatusChangeDto>());
         }
 
-        [Fact]
-        public async void ChangeTaskStatus_EvaluatedToClosed_ReturnsTheChangedTaskStatus()
+        [Theory]
+        [InlineData(1, "Task1", Status.Ended)]
+        public async void ChangeTaskStatus_EvaluatedToEnded_ReturnsTheChangedTaskStatus(long sessionId, string statusId, Status status)
         {
             //Arrange
-            var InitialSession = new[] { CreateRandomSession(1) };
-            repositoryStub.Setup(repo => repo.Get())
-                .Returns(InitialSession);
+            //Arrange the expected Result
+            var expectedTaskStatusChangeDtoResult = new List<TaskStatusChangeDto>() { new TaskStatusChangeDto() { Id = statusId, Status = status } };
+            var destinationStatus = new TaskStatusChangeDto()
+            {
+                Id = statusId,
+                Status = status
+            };
 
-            var Controller = new SessionService(repositoryStub.Object);
-            var firstTask = InitialSession[0].Tasks[0];
-
-            //Set the first Tasks Status to Evaluated
+            //Arrange the Mock Repository with a Session
+            var repoStub = new Mock<ILiteDbRepository<Session>>(MockBehavior.Strict);
+            var InitialSession = CreateRandomSession(1);
+            var firstTask = InitialSession.Tasks[0];
+            firstTask.Id = statusId;
             firstTask.Status = Status.Evaluated;
 
-            //Act
-            var newStatusEnded= new TaskStatusChangeDto()
-            {
-                Id = firstTask.Id,
-                Status = Status.Ended,
-            };
-            //output.WriteLine("Hello" + firstTask);
-            var (TaskChanged, modifiedTasks) = await Controller.ChangeTaskStatus(1, newStatusEnded);
-            //Assert.True(TaskChanged);
-            output.WriteLine("List" + modifiedTasks.Count);
-            //Assert that the TaskStatusChangeDto delivers an Ended Status
-            var resultTaskStatusChangeDto = modifiedTasks.Where(task => task.Id == firstTask.Id).ToList();
-            Assert.True(resultTaskStatusChangeDto[0].Status.Equals(Status.Ended));
-
-            //Assert that the Task's status inside the Database was changed to suspended
-            //var updatedRepositorySession = await Controller.GetSession(1);
-            //var updatedTask = updatedRepositorySession.Tasks[0];
-            //Assert.Equal(Status.Ended, updatedTask.Status);
-        }
-
-
-        [Fact]
-        public async Task<bool> ChangeSessionStatus_SuspendedToSuspended_ReturnsNoStatusChanges()
-        {
-            //Arrange
-            var InitialSession = new[] { CreateRandomSession(1) };
-            repositoryStub.Setup(repo => repo.Get())
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
                 .Returns(InitialSession);
 
-            var Controller = new SessionService(repositoryStub.Object);
-            var firstTask = InitialSession[0].Tasks[0];
+            repositoryStub.Setup(repo => repo.Update(
+                It.IsAny<Session>()
+            ))
+                .Returns(true);
 
-            //Set the first Tasks Status to Suspended
+            var service = new SessionService(repositoryStub.Object);
+
+            //Act
+            var (modified, modifiedTasks) = await service.ChangeTaskStatus(sessionId, destinationStatus);
+
+            //Assert 
+            //Assert that the delivered indicator whether changes were made is false, since no changes are expected
+            Assert.True(modified);
+            //Assert that the list of modified TaskStatusChangeDtos delivers a suspended Status
+            modifiedTasks.Should().BeEquivalentTo(expectedTaskStatusChangeDtoResult, options => options.ComparingByMembers<TaskStatusChangeDto>());
+        }
+
+        [Theory]
+        [InlineData(1, "Task1", Status.Ended)]
+        public async void ChangeTaskStatus_SuspendedToEnded_ReturnsTheChangedTaskStatus(long sessionId, string statusId, Status status)
+        {
+            //Arrange
+            //Arrange the expected Result
+            var expectedTaskStatusChangeDtoResult = new List<TaskStatusChangeDto>() { new TaskStatusChangeDto() { Id = statusId, Status = status } };
+            var destinationStatus = new TaskStatusChangeDto()
+            {
+                Id = statusId,
+                Status = status
+            };
+
+            //Arrange the Mock Repository with a Session
+            var repoStub = new Mock<ILiteDbRepository<Session>>(MockBehavior.Strict);
+            var InitialSession = CreateRandomSession(1);
+            var firstTask = InitialSession.Tasks[0];
+            firstTask.Id = statusId;
             firstTask.Status = Status.Suspended;
 
-            //Act
-            var newStatusSuspended = new TaskStatusChangeDto()
-            {
-                Id = firstTask.Id,
-                Status = Status.Suspended,
-            };
-
-            var (TaskChanged, modifiedTasks) = await Controller.ChangeTaskStatus(1, newStatusSuspended);
-            if (!TaskChanged)
-            {
-                return false;
-            }
-
-            //Assert that the TaskStatusChangeDto delivers a suspended Status
-            var resultTaskStatusChangeDto = modifiedTasks.Where(task => task.Id == firstTask.Id).ToList();
-            Assert.True(resultTaskStatusChangeDto[0].Status.Equals(Status.Suspended));
-
-            //Assert that the Task's status inside the Database was changed to suspended
-            var updatedRepositorySession = await Controller.GetSession(1);
-            var updatedTask = updatedRepositorySession.Tasks[0];
-            Assert.Equal(Status.Suspended, updatedTask.Status);
-
-            return true;
-        }
-
-        [Fact]
-        public async Task<Object> AddUserToSession_WithPopulatedRepo_ReturnsTrue()
-        {
-            //Arrange
-            var InitialSession = new[] { CreateRandomSession(1) };
-            repositoryStub.Setup(repo => repo.Get())
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
                 .Returns(InitialSession);
 
-            var Controller = new SessionService(repositoryStub.Object);
-            var InitialUsers = InitialSession[0].Users;
-            //Act
-            var newUser = new User { Id = "Vlad", Role = 0 };
-            var UserAdded = await Controller.AddUserToSession(1, newUser.Id, newUser.Role);
-            if (!UserAdded)
-            {
-                return false;
-            }
-            //Assert
-            var JoinedSessions = await Controller.GetSession(1);
-            var resultUsers = JoinedSessions.Users;
+            repositoryStub.Setup(repo => repo.Update(
+                It.IsAny<Session>()
+            ))
+                .Returns(true);
 
+            var service = new SessionService(repositoryStub.Object);
+
+            //Act
+            var (modified, modifiedTasks) = await service.ChangeTaskStatus(sessionId, destinationStatus);
+
+            //Assert 
+            //Assert that the delivered indicator whether changes were made is false, since no changes are expected
+            Assert.True(modified);
+            //Assert that the list of modified TaskStatusChangeDtos delivers a suspended Status
+            modifiedTasks.Should().BeEquivalentTo(expectedTaskStatusChangeDtoResult, options => options.ComparingByMembers<TaskStatusChangeDto>());
+        }
+
+
+        [Fact]
+        public async void AddUserToSession_WithPopulatedRepo_ReturnsTrue()
+        {
+            //Arrange
+            var newUser = new User { Id = "Vlad", Role = 0 };
+            var InitialSession = CreateRandomSession(1);
+
+            repositoryStub.Setup(repo => repo.GetFirstOrDefault(
+                It.IsAny<LiteDB.BsonExpression>()
+            ))
+                .Returns(InitialSession);
+
+            repositoryStub.Setup(repo => repo.Update(
+                It.IsAny<Session>()
+            ))
+                .Returns(true);
+
+            var Controller = new SessionService(repositoryStub.Object);
+            var InitialUsers = InitialSession.Users;
+
+            //Act
+            var UserAdded = await Controller.AddUserToSession(1, newUser.Id, newUser.Role);
+            var actualSession = await Controller.GetSession(1);
+            var resultUsers = actualSession.Users;
             var LastUser = resultUsers.Last<User>();
 
-            return !resultUsers.Equals(InitialUsers) && LastUser.Equals(newUser);
+            //Assert
+            Assert.True(UserAdded);
+            resultUsers.Last<User>().Should().BeEquivalentTo(newUser);
         }
 
         private Session CreateRandomSession(long sessionId)
@@ -222,6 +246,9 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Management.Controllers
             Users.Add(CreateRandomUser());
             IList<Task> Tasks = new List<Task>();
             Tasks.Add(CreateRandomTask());
+            Tasks.Add(CreateRandomTask());
+            Tasks.Add(CreateRandomTask());
+
 
             //Mockup estimations by each of the 3 random created users
             Estimation estimation1 = new Estimation { VoteBy = Users[0].Id, Complexity = rnd.Next(13) };
@@ -238,7 +265,7 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Management.Controllers
             {
                 Id = sessionId,
                 InviteToken = Guid.NewGuid().ToString(),
-                ExpiresAt = DateTime.Now.AddMinutes(30),
+                ExpiresAt = DateTime.Now.AddDays(1),
                 Tasks = Tasks,
                 Users = Users,
             };
@@ -272,27 +299,5 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Management.Controllers
                 Role = role,
             };
         }
-
-
-        /* Example Fact Check
-        [Fact]
-        public async Task GetDishesByCategory_WithGivenCategories_ReturnsTheCorrespondingCategoryDishes()
-        {
-            //Arrange
-            var Dishes = new[] { CreateRandomUser(), CreateRandomUser(), CreateRandomUser() };
-            var ExpectedCategories = new[] { Dishes[0].Category, Dishes[1].Category };
-            IList<Dish> ExpectedDishes = new List<Dish> { Dishes[0], Dishes[1] };
-            IList<string> Ids = new List<string>() { ExpectedCategories[0].FirstOrDefault().Id, ExpectedCategories[1].FirstOrDefault().Id };
-
-            var controller = new DishService(repositoryStub.Object);
-
-            repositoryStub.Setup(repo => repo.GetDishesByCategory(Ids))
-                .ReturnsAsync(ExpectedDishes);
-            //Act
-            var result = await controller.GetDishesByCategory(Ids);
-            //Assert
-            result.Should().BeEquivalentTo(ExpectedDishes, options => options.ComparingByMembers<Dish>());
-        }
-        */
     }
 }
