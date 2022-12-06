187d450740b0158f6c3905cc0e757cbd0e622173
moritzLanger <44358321+moritzLanger@users.noreply.github.com>
2022-12-06 09:19:04 +0100

    Update ISessionService.cs

--- dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/ISessionService.cs
+++ dotnet-estimation/dotnet-estimation/Templates/WebAPI/Devon4Net.Application.WebAPI.Implementation/Business/SessionManagement/Service/ISessionService.cs
@@ -1,6 +1,6 @@
 using Devon4Net.Application.WebAPI.Implementation.Domain.Entities;
 using Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Dtos;
-
+using ErrorOr;
 using LiteDB;
 
 namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement.Service
@@ -12,31 +12,31 @@ namespace Devon4Net.Application.WebAPI.Implementation.Business.SessionManagement
     {
         public Task<Session> GetSession(long id);
 
-        public Task<(bool, List<Devon4Net.Application.WebAPI.Implementation.Domain.Entities.Task>)> GetStatus(long sessionId);
+        public Task<ErrorOr<(bool, List<Domain.Entities.Task>)>> GetStatus(long sessionId);
 
         /// <summary>
         /// CreateSession
         /// </summary>
         /// <param name="sessionDto"></param>
         /// <returns></returns>
-        public Task<BsonValue> CreateSession(SessionDto sessionDto);
-        public Task<bool> InvalidateSession(long sessionId);
+        public Task<ErrorOr<BsonValue>> CreateSession(SessionDto sessionDto);
+        public Task<ErrorOr<bool>> InvalidateSession(long sessionId);
 
-        public Task<Estimation> AddNewEstimation(long sessionId, string taskId, string voteBy, int complexity);
+        public Task<ErrorOr<Estimation>> AddNewEstimation(long sessionId, string taskId, string voteBy, int complexity);
 
-        public Task<bool> RemoveUserFromSession(long id, String userId);
+        public Task<ErrorOr<bool>> RemoveUserFromSession(long id, String userId);
 
         /// <summary>
         /// Add an User to a given session
         /// </summary>
-        public Task<bool> AddUserToSession(long sessionId, string userId, Role role);
-        public Task<(bool, TaskDto?)> AddTaskToSession(long sessionId, TaskDto task);
+        public Task<ErrorOr<bool>> AddUserToSession(long sessionId, string userId, Role role);
+        public Task<ErrorOr<(bool, TaskDto?)>> AddTaskToSession(long sessionId, TaskDto task);
 
         /// <summary>
         /// Delete a Task
         /// </summary>
-        public Task<bool> DeleteTask(long sessionId, string taskId);
+        public Task<ErrorOr<bool>> DeleteTask(long sessionId, string taskId);
 
-        public Task<(bool, List<TaskStatusChangeDto>)> ChangeTaskStatus(long sessionId, TaskStatusChangeDto statusChange);
+        public Task<ErrorOr<(bool, List<TaskStatusChangeDto>)>> ChangeTaskStatus(long sessionId, TaskStatusChangeDto statusChange);
     }
 }
